624f679f4f3bd96a5024eb390b3512f5a08cd8b0
Fernando Coronel <ferchovas20@gmail.com>
2021-09-29 21:58:37 -0500

    Aplicacion de gifs con angular

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { SearchGiftsResponse, Gif } from '../interface/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private apiKeyGifs: string = '0xjc3qGCBWXnIF902AbpAVCOqfl5OCQJ';
+  private servicioURL: string = 'https://api.giphy.com/v1/gifs';
+  private _historial: string[]= [];
+
+  public resultados: Gif[] = [];
+
+    get historial(){
+      this._historial = this._historial.splice(0,20)
+      return [...this._historial];
+    }
+
+    constructor( private http:HttpClient ){
+      this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+      this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+    }
+
+    buscarGifs( query: string){
+      
+      query = query.trim().toLowerCase();
+      
+      if( !this._historial.includes( query )){
+        
+        this._historial.unshift ( query );
+        this._historial = this._historial.splice(0,20);
+
+        localStorage.setItem('historial', JSON.stringify(this._historial));
+        
+      }
+
+      const params = new HttpParams()
+      .set('api_key', this.apiKeyGifs)
+      .set('limit', '10')
+      .set('q', query);
+      
+      this.http.get<SearchGiftsResponse>(`${this.servicioURL}/search`,{ params })
+      .subscribe( ( resp ) => {
+        console.log( resp.data );
+        this.resultados = resp.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
+      })
+      
+    }
+
+
+}
